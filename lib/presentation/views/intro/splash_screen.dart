474418acbfc88fd135e21ee178981b7743590fe1
Abhishek Yewale <abhiyewale13402@gmail.com>
2026-02-04 10:15:31 +0530

    initial commit

--- lib/presentation/views/intro/splash_screen.dart
+++ lib/presentation/views/intro/splash_screen.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../../../utils/core/constant/assets.dart';
-import '../../../utils/core/constant/constants.dart';
 import '../../controllers/intro/splash_screen_controller.dart';
-
 
 class SplashScreen extends GetView<SplashController> {
   const SplashScreen({super.key});
@@ -13,49 +10,12 @@ class SplashScreen extends GetView<SplashController> {
   Widget build(BuildContext context) {
     controller.splashScreenLogic();
     return Scaffold(
-     // backgroundColor: colorBlack,
-      body: Stack(
-        children: [
-          // Background Image
-          Positioned.fill(
-            child: Image.asset(
-              Assets.getPNGImage("splash_background.png"),
-              fit: BoxFit.cover,
-            ),
-          ),
-
-          // Centered App Icon & Name using Stack
-          Positioned.fill(
-            child: Center(
-              child: Stack(
-                alignment:
-                    Alignment.bottomCenter, // Align text at bottom of the logo
-                children: [
-                  // App Icon
-                  Image.asset(
-                    Assets.getAppSettings('splash_logo_image.png'),
-                    height: 290, // Adjust size
-                    width: 290,
-                  ),
-
-                  // App Name placed inside the logo at the bottom
-                  Positioned(
-                    bottom: 10, // Adjust text positioning inside the logo
-                    child: Text(
-                      Constants.APP_NAME,
-                      //AppLocalizations.of(context)!.splashAppName,
-                      style: TextStyle(
-                        fontSize: 35,
-                        fontWeight: FontWeight.w500,
-                       // color: colorWhite,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+      // backgroundColor: colorBlack,
+      body: Center(
+        child: Text(
+          "Vanashree",
+          style: TextStyle(fontSize: 35, color: Colors.black),
+        ),
       ),
     );
   }
